c67fbc2b97af4603ba788375aa20c38037946fe7
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-12-20 16:40:04 +0800

    Update SR04_CONFIG.h with ultrasonic sensor implementation

--- source/esp32/SmartSprayer/SR04_CONFIG.h
+++ source/esp32/SmartSprayer/SR04_CONFIG.h
@@ -1,6 +1,23 @@
 #ifndef SR04_CONFIG_H
 #define SR04_CONFIG_H
 
-void initSR04();
+const int trigPin = 12;
+const int echoPin = 13;
+
+void initSR04() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+long readDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = duration * 0.034 / 2;
+  return distance;
+}
 
 #endif
