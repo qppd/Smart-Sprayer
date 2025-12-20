2605987ef530c511e818ccceaeade93831512a69
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-12-20 16:44:36 +0800

    Add serial command monitor for testing components

--- source/esp32/SmartSprayer/GSM_CONFIG.h
+++ source/esp32/SmartSprayer/GSM_CONFIG.h
@@ -1,6 +1,44 @@
 #ifndef GSM_CONFIG_H
 #define GSM_CONFIG_H
 
-void initGSM();
+#include <SoftwareSerial.h>
+
+SoftwareSerial sim(10, 11); // RX, TX
+
+String phoneNumbers[] = {"+1234567890", "+0987654321"}; // example numbers
+
+void initGSM() {
+  sim.begin(9600);
+  delay(1000);
+  sim.println("AT");
+  delay(1000);
+  // Assume OK
+}
+
+void sendSMS(String number, String message) {
+  sim.println("AT+CMGF=1");
+  delay(100);
+  sim.println("AT+CMGS=\"" + number + "\"");
+  delay(100);
+  sim.println(message);
+  delay(100);
+  sim.write(26); // Ctrl+Z
+}
+
+void sendSMSToAll(String message) {
+  for (int i = 0; i < 2; i++) {
+    sendSMS(phoneNumbers[i], message);
+    delay(5000); // delay between sends
+  }
+}
+
+void checkNetwork() {
+  sim.println("AT+CREG?");
+  delay(100);
+  // Response would be read, but for now, print to Serial
+  while (sim.available()) {
+    Serial.write(sim.read());
+  }
+}
 
 #endif
--- source/esp32/SmartSprayer/SmartSprayer.ino
+++ source/esp32/SmartSprayer/SmartSprayer.ino
@@ -4,6 +4,7 @@
 #include "SR04_CONFIG.h"
 
 void setup() {
+  Serial.begin(9600);
   initLCD();
   initGSM();
   initRELAY();
@@ -11,6 +12,48 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    
+    if (command == "operate-relay1_on") {
+      operateRELAY(RELAY_1, true);
+      Serial.println("Relay 1 turned ON");
+    } else if (command == "operate-relay1_off") {
+      operateRELAY(RELAY_1, false);
+      Serial.println("Relay 1 turned OFF");
+    } else if (command == "operate-relay2_on") {
+      operateRELAY(RELAY_2, true);
+      Serial.println("Relay 2 turned ON");
+    } else if (command == "operate-relay2_off") {
+      operateRELAY(RELAY_2, false);
+      Serial.println("Relay 2 turned OFF");
+    } else if (command == "send-sms") {
+      sendSMS("+1234567890", "Test SMS from Smart Sprayer");
+      Serial.println("SMS sent");
+    } else if (command == "send-sms-to-all") {
+      sendSMSToAll("Test SMS to all from Smart Sprayer");
+      Serial.println("SMS sent to all");
+    } else if (command == "check-network") {
+      checkNetwork();
+      Serial.println("Network check initiated");
+    } else if (command == "get-distance1") {
+      long dist = readDistance();
+      Serial.print("Distance 1: ");
+      Serial.print(dist);
+      Serial.println(" cm");
+    } else if (command == "get-distance2") {
+      long dist = readDistance();
+      Serial.print("Distance 2: ");
+      Serial.print(dist);
+      Serial.println(" cm");
+    } else if (command == "test-display") {
+      clearLCD();
+      setLCDText("Test Display", 0, 0);
+      setLCDText("Smart Sprayer", 0, 1);
+      Serial.println("LCD test displayed");
+    } else {
+      Serial.println("Unknown command");
+    }
+  }
 }
