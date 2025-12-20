39d49d758b7f293afdaac58624f8f8b1f7bfb4d3
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-12-20 16:38:12 +0800

    Update LCD and RELAY config headers with implementation

--- source/esp32/SmartSprayer/RELAY_CONFIG.h
+++ source/esp32/SmartSprayer/RELAY_CONFIG.h
@@ -1,6 +1,35 @@
 #ifndef RELAY_CONFIG_H
 #define RELAY_CONFIG_H
 
-void initRELAY();
+uint16_t RELAY_1 = 4;
+uint16_t RELAY_2 = 5;
+
+//-----------------------------------------------------------------
+//FUNCTION FOR SETTING RELAY PIN MODE------------------------------
+//-----------------------------------------------------------------
+void initRELAY(){
+  pinMode(RELAY_1, OUTPUT);
+  pinMode(RELAY_2, OUTPUT);
+}
+
+//-----------------------------------------------------------------
+//FUNCTION FOR OPERATING RELAY-------------------------------------
+//-----------------------------------------------------------------
+void operateRELAY(uint16_t RELAY, boolean OPENED) {
+  if (OPENED)
+    digitalWrite(RELAY, LOW);
+  else
+    digitalWrite(RELAY, HIGH);
+}
+
+//-----------------------------------------------------------------
+//FUNCTION FOR OPERATING SOLID STATE RELAY-------------------------
+//-----------------------------------------------------------------
+void operateSSR(uint16_t RELAY, boolean OPENED) {
+  if (OPENED)
+    digitalWrite(RELAY, HIGH);
+  else
+    digitalWrite(RELAY, LOW);
+}
 
 #endif
