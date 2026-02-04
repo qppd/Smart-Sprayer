d7f5492e9b45a85f61300f630d1b179dc287da77
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2026-02-04 11:52:26 +0800

    Change app appearance mode to light

--- source/rpi/SmartSprayer/ui/main_ui.py
+++ source/rpi/SmartSprayer/ui/main_ui.py
@@ -34,7 +34,7 @@ class SmartSprayerUI(ctk.CTk):
         self.geometry("1400x900")
         
         # Set theme
-        ctk.set_appearance_mode("dark")
+        ctk.set_appearance_mode("light")
         ctk.set_default_color_theme("green")
         
         # Initialize backend components
