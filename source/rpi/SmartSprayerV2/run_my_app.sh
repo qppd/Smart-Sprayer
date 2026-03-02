02a47df92e0c300749ae9a3dd9e3c701b09032b1
qppd <quezon.province.pd@gmail.com>
2026-03-02 10:32:19 +0800

    Update by the team

---
+++ source/rpi/SmartSprayerV2/run_my_app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# wait a few seconds for HDMI display and USB touch to initialize
+sleep 5
+
+# run the Python GUI
+/usr/bin/python3 /home/zairafelideracio/Smart-Sprayer/source/rpi/SmartSprayerV2/run_gui.py
